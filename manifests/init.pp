81b4f0fc3b0928d0300664f7771641b32216bae4
Gabriel Lesperance <gabriel@wavo.me>
2011-09-27 14:42:13 -0400

    Uses echo instead of add-apt-repository

--- manifests/init.pp
+++ manifests/init.pp
@@ -23,7 +23,7 @@ class mongodb {
 	
 	exec { "10gen-apt-repo":
 		path => "/bin:/usr/bin",
-		command => "add-apt-repository '${mongodb::params::repository}'",
+		command => "echo '${mongodb::params::repository}' >> /etc/apt/sources.list",
 		unless => "cat /etc/apt/sources.list | grep 10gen",
 		require => Package["python-software-properties"],
 	}
